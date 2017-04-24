0cb70de2d4fe7f7fac8aaba660bd7f776eae573a
3hdeng <3hdeng@gmail.com>
2017-04-24 08:29:37 +0000

    chmod -x

--- forPython2/segment.py
+++ forPython2/segment.py
@@ -9,11 +9,11 @@ def main():
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     # jieba custom setting.
-    jieba.set_dictionary('jieba_dict/dict.txt.big')
+    jieba.set_dictionary('../jieba_dict/dict.txt.big')
 
     # load stopwords set
     stopwordset = set()
-    with io.open('jieba_dict/stopwords.txt','r',encoding='utf-8') as sw:
+    with io.open('../jieba_dict/stopwords.txt','r',encoding='utf-8') as sw:
         for line in sw:
             stopwordset.add(line.strip('\n'))
 
