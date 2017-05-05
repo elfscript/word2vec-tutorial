e69900a7e21cf6c80013a8b117d5a5048005017b
3hdeng <3hdeng@gmail.com>
2017-05-05 10:29:11 +0000

    model.wv.vocab,( k=wd, v=int?)

--- forPython2/wv2syn.py
+++ forPython2/wv2syn.py
@@ -6,6 +6,14 @@ import logging
 import io
 import sys
 
+""" http://stackoverflow.com/questions/35596031/gensim-word2vec-find-number-of-words-in-vocabulary
+The vocabulary is in the vocab field of the Word2Vec model's wv property, as a dictionary, with the keys being each token (word). So it's just the usual Python for getting a dictionary's length:
+
+len(w2v_model.wv.vocab)
+(In older gensim versions before 0.13, vocab appeared directly on the model. So you would use w2v_model.vocab instead of w2v_model.wv.vocab.)
+"""
+
+
 def main():
 	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 	model = models.Word2Vec.load('med250.model.bin')
@@ -13,14 +21,24 @@ def main():
 	#print(type(v1)) <type 'numpy.ndarray'>
         print(len(v1), v1[0:5], v1[-5:-1])
         print( u",".join(wv2syn(model.wv, u'天空', 5)) )
+        i=0
+        for wd in model.wv.vocab:
+	    print(wd)
+            i +=1
+            if i >3 :
+               break
+             
+               
+
+
         x=wv2ant(model.wv, u'天空', 3, -0.2)
         print(x)
 
 def wv2syn(wv, wd, topn, th=0.65):
    try:
-       vs= wv.most_similar(positive=[wd],topn = topn) 
+       # vs= wv.most_similar(positive=[wd],topn = topn) 
+       vs=wv.similar_by_word(wd, topn=topn)
        return [t[0] for t in vs if t[1]>=th]
-       return u",".join(x)
        #how to set threshold to filter out low-similarity words ?
        #say, to chosse only top 3 words haveing similarity > 0.9
  
